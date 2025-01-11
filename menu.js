5a6b5d35712e03455cc34aff185c59a03e4c9144
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-01-11 11:53:48 -0300

    Versao base - multiplos bugs
    em fase de testes

---
+++ menu.js
@@ -0,0 +1,17 @@
+let menu_lateral = document.getElementById('menu-lateral');
+let menu_btn = document.getElementById('button-menu')
+let menu_btn_open = document.getElementById('button-menu--open')
+
+/* Função para abrir o menu */
+menu_btn.addEventListener('click', ()=>{
+    menu_lateral.style.display = 'flex';
+    menu_btn.style.display = 'none';
+    menu_btn_open.style.display = 'flex';
+});
+
+/* Função para fechar o menu */
+menu_btn_open.addEventListener('click', ()=>{
+    menu_lateral.style.display = 'none';
+    menu_btn.style.display = 'flex';
+    menu_btn_open.style.display = 'none';
+})
