ade8ed4a1d77bb8d4ce6b89a858c907dfa5faba9
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-01-17 20:23:50 -0300

    Sobre nós, imagens, responsividade
    Adicionei a página sobre nós;
    Substituí as imagens.webp por jpg para ter mais qualidade;
    Ajustei a responsividade do menu;
    Criei o arquivo faqs.html

--- js/dataBase.js
+++ js/dataBase.js
@@ -1,71 +1,71 @@
 export const Products = [
     {
-        img: '../imgs/prods/rosa01.webp',
+        img: '../imgs/prods/rosa01.jpg',
         title: 'Buquê de rosas com 30 unidades',
         price: '450,00'
     },
     {
-        img: '../imgs/prods/rosa02.webp',
+        img: '../imgs/prods/rosa02.jpg',
         title: 'Buquê de rosas com 20 unidades',
         price: '260,00'
     },
     {
-        img: '../imgs/prods/rosa03.webp',
+        img: '../imgs/prods/rosa03.jpg',
         title: 'Buquê de rosas com 12 unidades',
         price: '198,00'
     },
     {
-        img: '../imgs/prods/rosa04.webp',
+        img: '../imgs/prods/rosa04.jpg',
         title: 'Buquê de rosas Spray',
         price: '900,00'
     },
     {
-        img: '../imgs/prods/rosa05.webp',
+        img: '../imgs/prods/rosa05.jpg',
         title: 'Buquê de Rosas Colombianas com 12 unidades',
         price: '218,00'
     },
     {
-        img: '../imgs/prods/rosa06.webp',
+        img: '../imgs/prods/rosa06.jpg',
         title: 'Buquê de Rosas Colombianas com 6 unidades',
         price: '139,00'
     },
     {
-        img: '../imgs/prods/rosa07.webp',
+        img: '../imgs/prods/rosa07.jpg',
         title: 'Buquê de rosas colombianas com 3 unidades',
         price: '79,00'
     },
     {
-        img: '../imgs/prods/rosa08.webp',
+        img: '../imgs/prods/rosa08.jpg',
         title: 'Buquê de rosas colombianas com 20 unidades',
         price: '370,00'
     },
     {
-        img: '../imgs/prods/rosa09.webp',
+        img: '../imgs/prods/rosa09.jpg',
         title: 'Ramalhete de Rosas Champagne',
         price: '99,00'
     },
     {
-        img: '../imgs/prods/rosa10.webp',
+        img: '../imgs/prods/rosa10.jpg',
         title: 'Ramalhete com 6 Rosas vermelhas',
         price: '99,00'
     },
     {
-        img: '../imgs/prods/rosa11.webp',
+        img: '../imgs/prods/rosa11.jpg',
         title: 'Ramalhete de Girassol',
         price: '58,00'
     },
     {
-        img: '../imgs/prods/rosa12.webp',
+        img: '../imgs/prods/rosa12.jpg',
         title: 'Ramalhete de Rosas Brancas',
         price: '98,00'
     },
     {
-        img: '../imgs/prods/girassol02.webp',
+        img: '../imgs/prods/girassol02.jpg',
         title: 'Arranjo de Girassol com 6 unidades',
         price: '174,00'
     },
     {
-        img: '../imgs/prods/girassol03.webp',
+        img: '../imgs/prods/girassol03.jpg',
         title: 'Arranjo de Girassol com 3 unidades',
         price: '160,00'
     }
