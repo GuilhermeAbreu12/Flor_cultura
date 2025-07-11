e233f0684afc834cbc9a8366c7661fec103dc8a2
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-07-10 22:25:48 -0300

    feat(dataBase): adds the 'amount' parameter
    The 'amount' parameter in the 'dataBase.js' file is essential for shopping cart functionality. This parameter is responsible for counting the products added to the shopping cart for each product type.

--- js/dataBase.js
+++ js/dataBase.js
@@ -2,71 +2,85 @@ export const Products = [
     {
         img: '../imgs/prods/rosa01.jpg',
         title: 'Buquê de rosas com 30 unidades',
-        price: '450,00'
+        price: '450,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa02.jpg',
         title: 'Buquê de rosas com 20 unidades',
-        price: '260,00'
+        price: '260,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa03.jpg',
         title: 'Buquê de rosas com 12 unidades',
-        price: '198,00'
+        price: '198,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa04.jpg',
         title: 'Buquê de rosas Spray',
-        price: '900,00'
+        price: '900,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa05.jpg',
         title: 'Buquê de Rosas Colombianas com 12 unidades',
-        price: '218,00'
+        price: '218,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa06.jpg',
         title: 'Buquê de Rosas Colombianas com 6 unidades',
-        price: '139,00'
+        price: '139,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa07.jpg',
         title: 'Buquê de rosas colombianas com 3 unidades',
-        price: '79,00'
+        price: '79,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa08.jpg',
         title: 'Buquê de rosas colombianas com 20 unidades',
-        price: '370,00'
+        price: '370,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa09.jpg',
         title: 'Ramalhete de Rosas Champagne',
-        price: '99,00'
+        price: '99,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa10.jpg',
         title: 'Ramalhete com 6 Rosas vermelhas',
-        price: '99,00'
+        price: '99,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa11.jpg',
         title: 'Ramalhete de Girassol',
-        price: '58,00'
+        price: '58,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/rosa12.jpg',
         title: 'Ramalhete de Rosas Brancas',
-        price: '98,00'
+        price: '98,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/girassol02.jpg',
         title: 'Arranjo de Girassol com 6 unidades',
-        price: '174,00'
+        price: '174,00',
+        amount: 0
     },
     {
         img: '../imgs/prods/girassol03.jpg',
         title: 'Arranjo de Girassol com 3 unidades',
-        price: '160,00'
+        price: '160,00',
+        amount: 0
     }
 ]
