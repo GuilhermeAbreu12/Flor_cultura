402ed481cc2ce9af851d63a5049c733d1bbe4136
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-07-10 22:28:09 -0300

    feat(products.js): Shopping cart system
    Creates the shopping cart operating system

--- js/products.js
+++ js/products.js
@@ -16,23 +16,32 @@ const Gerberas = document.getElementById('Gerberas')
 import { Products } from "./dataBase.js";
 import { searchbar } from "./searchbar.js";
 import { filtrar } from "./searchbar.js";
+// import { totalProducts } from "./dataBase.js"
 let content = [];
 export let c = 0;
 // Gerando dinamicamente
 for(c in Products){
     content += `
         <div class="products" tabindex="3">
-            <div class="products__img">
-                <img src="${Products[c].img}" alt="imagem da flor ${Products[c].title}">
-            </div>
+            <div class="products-head">
+                <div class="products__img">
+                    <img src="${Products[c].img}" alt="imagem da flor ${Products[c].title}">
+                </div>
+                <div class="products-amount-div">
+                    <p class="products-amount-div__number"></p>
+                </div>
+            </div>    
             <div class="products__title">
                 <p>${Products[c].title}</p>
             </div>
             <button class="products__price" tabindex="3">
             <h5>R$ ${Products[c].price}</h5>
             </button>
+            <div class="products__quantityBar">
+                <button class="removeButton">-</button>
+                <button class="addButton">+</button>
+            </div>
         </div>
-        
     `
 }
 let nothingMore = `
@@ -87,3 +96,79 @@ Gerberas.addEventListener('click', ()=>{
     filtrar()
 })
 main__product_box.innerHTML = content + nothingMore
+
+// Sistema de compras
+const buyNumber = document.getElementById('buyNumber__Number');
+let totalProducts = 0;
+buyNumber.innerText = totalProducts;
+
+const graySpace = document.getElementsByClassName('products-amount-div');
+
+const shoppingCartIcon = document.getElementById('shopping_cart__cart')
+
+const products__quantityBar = document.querySelectorAll('.products__quantityBar');
+const buyButton = document.querySelectorAll('.products__price').forEach((button, i) => {
+    button.addEventListener('click', () =>{
+        if (Products[i].amount === 0){
+            products__quantityBar[i].classList.add('active');
+            graySpace[i].classList.add('active')            
+        }
+        adicionarDados(i)
+        atualizarDados()
+    })
+})
+
+const addButton = document.querySelectorAll('.addButton').forEach((button, i) => {
+    button.addEventListener('click', () => {
+        adicionarDados(i)
+        atualizarDados()
+    })
+})
+
+const removeButton = document.querySelectorAll(".removeButton").forEach((button, i) => {
+    button.addEventListener('click', ()=>{
+        if (Products[i].amount === 1){
+            products__quantityBar[i].classList.remove('active');
+            graySpace[i].classList.remove('active');
+        }
+        if ((totalProducts >= 0) && (totalProducts <= 999)){
+            Products[i].amount --;
+            totalProducts --;
+        }
+        atualizarDados()
+    })
+})
+
+const ShoppingCartItems = [];
+
+function atualizarDados(){
+    buyNumber.innerText = totalProducts;
+    if (totalProducts > 0) shoppingCartIcon.style.fill = 'white'; 
+    else shoppingCartIcon.style.fill = 'none';
+    
+    const buyNumber__Number = document.querySelectorAll('.products-amount-div__number').forEach((number, i) => { 
+        number.innerHTML = `${Products[i].amount}`;
+    });
+    Products.forEach(product => {
+        if (product.amount > 0){
+            ShoppingCartItems.push(product.title, product.price)
+        }
+    });
+    console.log(ShoppingCartItems);
+}
+function adicionarDados(i){
+    if ((totalProducts >= 0) && (totalProducts < 999)){
+        Products[i].amount ++;
+        totalProducts ++;
+    }
+}
+
+// Shopping cart - page
+const ShoppingCartList = document.querySelector('#ShoppingCartList');
+
+if (ShoppingCartList){
+    ShoppingCartItems.innerHTML += `
+        <p>${ShoppingCartItems}</p>
+    `
+}
+// Estou tentando usar este mesmo script para rodar na página de carrinho, preciso de uma forma de separar os títulos, imagens, preços, etc, dentro da const ShoppingCartList para que possa formatar corretamente na página
